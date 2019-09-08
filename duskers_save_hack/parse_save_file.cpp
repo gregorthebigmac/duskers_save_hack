7539a339918b1fda96aa7f6e5f826d5af7a175e3
Gregor <gregorthebigmac@gmail.com>
2019-09-08 14:15:41 -0500

    had to quit mid-stride.

--- duskers_save_hack/parse_save_file.cpp
+++ duskers_save_hack/parse_save_file.cpp
@@ -8,14 +8,38 @@ parse_save_file::parse_save_file() {
 parse_save_file::~parse_save_file() {}
 
 bool parse_save_file::is_config_file_present() {
-
-	return false;
+	bool config_success = false;
+	cout << "Searching working directory for " << _config_filename << "...";
+	std::ifstream fin;
+	fin.open(_config_filename.c_str());
+	if (fin.is_open()) {
+		cout << "[OK]";
+		config_success = true;
+		fin.close();
+	}
+	else {
+		cout << "[FAIL]" << endl << read_config_file << " not found! Quitting..." << endl;
+		config_success = false;
+	}
+	return config_success;
 }
 
 uint8_t parse_save_file::read_config_file() {
-	return uint8_t();
+	uint8_t read_config_file_success = 0;	// 0 means success; all other numbers correspond to an error.
+	std::ifstream fin;
+	fin.open(_config_filename.c_str());
+	if (fin.is_open()) {
+		while (!fin.eof()) {
+			string temp;
+			std::getline(fin, temp);
+			m_config_file_contents.push_back(temp);
+		}
+		for (int i = 0; i < m_config_file_contents.size(); i++) {
+			
+		}
+	}
 }
 
-uint8_t parse_save_file::read_file_contents() {
+uint8_t parse_save_file::read_save_file_contents() {
 	return uint8_t();
 }
--- duskers_save_hack/parse_save_file.h
+++ duskers_save_hack/parse_save_file.h
@@ -1,9 +1,13 @@
 #pragma once
+#include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 
 using std::vector;
 using std::string;
+using std::cout;
+using std::endl;
 
 class parse_save_file {
 public:
@@ -11,8 +15,11 @@ public:
 	~parse_save_file();
 	bool is_config_file_present();
 	uint8_t read_config_file();
-	uint8_t read_file_contents();
+	uint8_t read_save_file_contents();
 private:
 	string _config_filename;
+	vector<string> m_config_file_contents;
 	vector<string> m_file_contents;
+	uint8_t max_fuel;
+	int max_scrap;
 };
