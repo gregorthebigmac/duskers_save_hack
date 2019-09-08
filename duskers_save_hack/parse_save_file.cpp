07c1d2d36e20b94e20bebac168c8120a88ce502e
Gregor <gregorthebigmac@gmail.com>
2019-09-08 08:47:39 -0500

    made config file and added initial data

--- duskers_save_hack/parse_save_file.cpp
+++ duskers_save_hack/parse_save_file.cpp
@@ -1,6 +1,21 @@
 #include "pch.h"
 #include "parse_save_file.h"
 
+parse_save_file::parse_save_file() {
+	_config_filename = "config.txt";
+}
 
-parse_save_file::parse_save_file() {}
 parse_save_file::~parse_save_file() {}
+
+bool parse_save_file::is_config_file_present() {
+
+	return false;
+}
+
+uint8_t parse_save_file::read_config_file() {
+	return uint8_t();
+}
+
+uint8_t parse_save_file::read_file_contents() {
+	return uint8_t();
+}
--- duskers_save_hack/parse_save_file.h
+++ duskers_save_hack/parse_save_file.h
@@ -9,7 +9,10 @@ class parse_save_file {
 public:
 	parse_save_file();
 	~parse_save_file();
+	bool is_config_file_present();
+	uint8_t read_config_file();
+	uint8_t read_file_contents();
 private:
+	string _config_filename;
 	vector<string> m_file_contents;
 };
-
